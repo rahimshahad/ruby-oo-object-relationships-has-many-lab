cdd797c38f45ac043bc3c40bb963fb059c363f59
rahimshahad <rahim.shahad2@gmail.com>
2021-01-15 01:26:06 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+require "pry"
+class Post
+    @@all = Array.new
+    attr_accessor :title, :author
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def author_name
+        if @author == nil
+            nil
+        else
+            @author.name
+        end
+    end
+end
